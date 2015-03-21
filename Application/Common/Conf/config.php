0c92e2a97c379f43204af07e31f5bb855bd8714c
cornerljj <395005280@qq.com>
2015-03-21 15:35:17 +0800

    44

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -42,7 +42,7 @@ return array(
     'DB_HOST'   => '127.0.0.1', // 服务器地址
     'DB_NAME'   => 'cysh', // 数据库名
     'DB_USER'   => 'root', // 用户名
-    'DB_PWD'    => 'root',  // 密码
+    'DB_PWD'    => '123',  // 密码
     'DB_PORT'   => '3306', // 端口
     'DB_PREFIX' => 'cysh_', // 数据库表前缀
 
--- Application/User/Conf/config.php
+++ Application/User/Conf/config.php
@@ -15,5 +15,5 @@
 define('UC_APP_ID', 1); //应用ID
 define('UC_API_TYPE', 'Model'); //可选值 Model / Service
 define('UC_AUTH_KEY', 'Pym.zThG!q:eBV`Yg3Etx|QKW7L52C#v-<XZbd*9'); //加密KEY
-define('UC_DB_DSN', 'mysqli://root:root@127.0.0.1:3306/cysh'); // 数据库连接，使用Model方式调用API必须配置此项
+define('UC_DB_DSN', 'mysqli://root:123@127.0.0.1:3306/cysh'); // 数据库连接，使用Model方式调用API必须配置此项
 define('UC_TABLE_PREFIX', 'cysh_'); // 数据表前缀，使用Model方式调用API必须配置此项
